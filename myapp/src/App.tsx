b3fea8669e28b76b790bbefc4672e9ae3fd39fe8
CiceroIsaac2 <ciceroisaacsc@gmail.com>
2023-11-07 13:47:55 -0300

    Transformação em ViteApp e criação de componente básico do header
    Falta concluir header

---
+++ myapp/src/App.tsx
@@ -0,0 +1,10 @@
+import Header from "./components/header"/*Importando componente deo cabeçalho do site*/
+
+export default function App() {
+  return(
+    <div>
+      <Header />
+    </div>
+  )
+}
+
---
+++ myapp/src/components/header.tsx
@@ -0,0 +1,21 @@
+export default function Header(){
+
+    return(
+
+        <div className="w-full bg-cinzaClaro">
+            <div className="mx-auto flex max-w-[1600px] justify-between">
+                <div className="">
+                    <a href="" className="">Métodos Contraceptivos</a>
+                    <a href="" className="">IST’S</a>
+                    <a href="" className="">Sobre</a>
+                    <a href="" className="">Quem Somos</a>
+                </div>
+                <div className="">
+                    <input type="text" placeholder="Pesquisar..." className="" />
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
