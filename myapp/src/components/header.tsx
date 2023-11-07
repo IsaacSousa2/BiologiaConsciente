9242ded82aa4f7aa52e280a9fe849ff0642b37d7
CiceroIsaac2 <ciceroisaacsc@gmail.com>
2023-11-07 16:32:00 -0300

    Fim do cabeçalho nao responivo

--- myapp/src/components/header.tsx
+++ myapp/src/components/header.tsx
@@ -2,16 +2,27 @@ export default function Header(){
 
     return(
 
-        <div className="w-full bg-cinzaClaro">
-            <div className="mx-auto flex max-w-[1600px] justify-between">
-                <div className="">
-                    <a href="" className="">Métodos Contraceptivos</a>
-                    <a href="" className="">IST’S</a>
-                    <a href="" className="">Sobre</a>
-                    <a href="" className="">Quem Somos</a>
+        <div className="w-full bg-cinzaClaro h-16 flex">
+            <div className="mx-auto flex max-w-[1600px] w-full justify-between">
+                <div className="text-xl flex gap-9 items-center md:hidden">
+                    <div>
+                    <a href="#" className="">Métodos Contraceptivos</a>
+                    </div>
+                    <div>
+                    <a href="#" className="">IST’S</a>
+                    </div>
+                    <div>
+                    <a href="#" className="">Sobre</a>
+                    </div>
+                    <div>
+                    <a href="#" className="">Quem Somos</a>
+                    </div>
                 </div>
                 <div className="">
-                    <input type="text" placeholder="Pesquisar..." className="" />
+                    
+                </div>
+                <div className="items-center flex ">
+                    <input type="text" placeholder="Pesquisar..." className="w-52"/>
                 </div>
             </div>
         </div>
